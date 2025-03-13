470de81610095e0f7330f6083539c910d0893974
Petr <tikhonkovpetr@mail.ru>
2025-03-13 19:48:46 +0300

    Version 1.1

---
+++ src/ObjectLibrary.kt
@@ -0,0 +1,18 @@
+abstract class ObjectLibrary(val id: Int, val name: String, var available: Boolean) : FullInfoInterface {
+    fun briefInformation() {
+        println("ID: ${id}| ${this.name} доступна: ${yesOrNot(available)}")
+    }
+
+    fun returnObject() {
+        if (!available) {
+            println("Объект ${this.name} с ID: ${this.id} вернули")
+            available = !available
+        } else {
+            println("Этот объект находится в библиотеке!")
+        }
+    }
+
+    fun yesOrNot(avail: Boolean): String {
+        return (if (avail) "Да" else "Нет")
+    }
+}
